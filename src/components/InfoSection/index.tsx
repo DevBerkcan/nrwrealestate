e9b0b87d77ebb58e5aed50498625dc0b12e99662
Turkes, Medin <medin.turkes@realcore.de>
2025-05-14 15:39:52 +0200

    Fix text break points

--- src/components/InfoSection/index.tsx
+++ src/components/InfoSection/index.tsx
@@ -20,7 +20,7 @@ const InfoSection: React.FC<InfoSectionProps> = ({
   return (
     <section
       className={cn(
-        "py-10 flex flex-col-reverse [@media(min-width:1350px)]:flex-row gap-20 lg:gap-28",
+        "py-10 flex flex-col-reverse [@media(min-width:1350px)]:flex-row gap-20 lg:gap-20",
         className
       )}
     >
@@ -38,7 +38,7 @@ const InfoSection: React.FC<InfoSectionProps> = ({
 
       {/* Text Section */}
       <div
-        className={`w-full text-normal flex-1 flex flex-col pt-10 font-gothic `}
+        className={`w-full text-normal flex-1 flex flex-col pt-4 font-gothic `}
       >
         {title && (
           <h2 className="font-merriweather font-[400] text-center md:text-left text-[#1a1a1a] text-[40px] leading-[32px] mb-2">
@@ -46,35 +46,47 @@ const InfoSection: React.FC<InfoSectionProps> = ({
           </h2>
         )}
         {subtitle && (
-          <h2 className="font-gothic font-bold text-center md:text-left text-[#1a1a1a] text-normal leading-[52px] mb-16">
+          <h2 className="font-gothic font-bold text-center md:text-left text-[#1a1a1a] text-normal leading-[52px] mb-10">
             {subtitle}
           </h2>
         )}
         <p className="  mb-10">
-        Wir sind kontinuierlich auf der Suche nach attraktiven Immobilien in Wuppertal und Umgebung.
+          Wir sind kontinuierlich auf der Suche nach attraktiven Immobilien in
+          Wuppertal und Umgebung.
         </p>
         <p className=" mb-10">
           <>
-          Dank unserer langjährigen Expertise bewerten wir Immobilien verschiedenster Baujahre und Zustände präzise
-    und erkennen verborgene Potenziale.
-    <br />
-    Unser Ankaufsprofil umfasst sowohl sanierte Mehrfamilienhäuser als auch entwicklungsbedürftige Bestandsobjekte
-    mit hohem Aufwertungspotenzial.
+            Dank unserer langjährigen Expertise können wir Immobilien
+            inunterschiedlichsten <br />
+            Zuständen und aus allen Baujahrklassen präzise bewerten und
+            verborgene
+            <br /> Potenziale erkennen.
+            <br />
+            <br />
+            Unser Ankaufsprofil umfasst sowohl sanierte Mehrfamilienhäuser als
+            <br />
+            auch entwicklungsbedürftige Bestandsobjekte mit hohem
+            Aufwertungspotenzial.
           </>
         </p>
         <p className=" font-semibold mb-10">Unser Fokus:</p>
         <p className=" mb-10">
-        Wohnimmobilien wie Mehrfamilienhäuser, Wohn- und Geschäftshäuser, Wohnanlagen sowie Grundstücke.
+          Immobilien im Wohnbereich, einschließlich Mehrfamilienhäuser,
+          <br />
+          Wohn- und Geschäftshäuser, Wohnanlagen sowie Grundstücke.
         </p>
         <p className=" mb-10">
-        Bestandsobjekte mit Entwicklungspotenzial – etwa durch Leerstand oder Sanierungsbedarf.
+          Bestandsobjekte mit Entwicklungspotenzial - etwa durch Leerstand oder
+          <br />
+          Sanierungsbedarf.
         </p>
         <p className=" mb-10">
-        Wir garantieren eine diskrete, transparente und zügige Abwicklung.
+          Wir garantieren eine diskrete, transparente und zügige Abwicklung.
         </p>
         <p className=" mb-10">
-        Besuchen Sie gerne unser Ankaufsprofil, um mehr über unsere Kriterien zu erfahren.  
-        Wir freuen uns auf eine mögliche Zusammenarbeit.
+          Besuchen Sie gerne unser Ankaufsprofil, um mehr über unsere Kriterien
+          zu erfahren.
+          <br /> Wir freuen uns auf eine mögliche Zusammenarbeit.
         </p>
 
         {addButton && (
