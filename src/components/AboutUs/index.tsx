9302241f4edcf9dcbf6c06351e387787cc632c57
DevBerkcan <atesoglu.berkcan@outlook.com>
2025-05-15 11:14:46 +0200

    fix: about us section

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -20,20 +20,20 @@ const AboutSection: React.FC<AboutSectionProps> = ({
   return (
     <section
       className={cn(
-        "flex flex-col [@media(min-width:1350px)]:flex-row gap-98 text-[#1a1a1a]",
+        "flex flex-col [@media(min-width:1350px)]:flex-row gap-20 text-[#1a1a1a]",
         className
       )}
     >
       {/* Text Section */}
-      <div className="flex-1 text-left">
-        <h2 className="text-[40px] font-merriweather font-normal leading-normal text-center lg:text-left mb-16">
+      <div className="w-full [@media(min-width:1350px)]:w-[729px] h-auto text-left pt-10">
+        <h2 className="text-[40px] font-merriweather font-normal leading-normal text-center lg:text-left mb-20">
           {title}
         </h2>
-        <div className={`flex flex-col  font-gothic gap-6 md:gap-12`}>
+        <div className="flex flex-col font-gothic gap-10 md:gap-12">
           {content.map((para, ind) => (
             <p
               key={ind}
-              className={`text-[18px] text-[#1a1a1a] font-gothic font-normal subpixel-antialiased tracking-wide leading-relaxed`}
+              className="text-[18px] text-[#1a1a1a] font-normal subpixel-antialiased tracking-wide leading-relaxed"
             >
               {para}
             </p>
@@ -42,12 +42,12 @@ const AboutSection: React.FC<AboutSectionProps> = ({
       </div>
 
       {/* Image Section */}
-      <div className="max-w-[680px] [@media(min-width:1350px)]:w-[726px]">
+      <div className="w-full [@media(min-width:1350px)]:w-[726px] max-w-[726px]">
         <Image
-          className="w-full  h-auto object-cover"
+          className="w-full h-auto object-cover"
           alt="About Section Image"
           src={imageSrc}
-          width={680}
+          width={726}
           height={696}
           layout="intrinsic"
           priority
