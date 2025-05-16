ef1ef4191b85ec01c19c121e1011c67e6681bbbc
Turkes, Medin <medin.turkes@realcore.de>
2025-05-16 15:03:49 +0200

    Adjust contact page

--- src/app/Impressum/page.tsx
+++ src/app/Impressum/page.tsx
@@ -38,7 +38,7 @@ const Impressum = () => {
               Kontakt
             </h2>
             <p className="text-gray-700 leading-relaxed">
-              Geschäftsführer & Gesellschafter: <span className="font-medium">Oliver Jacobi</span><br /><br />
+              <span className="font-medium">Oliver Jacobi</span><br /><br />
               <span className="flex items-center mt-1">
                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
@@ -80,7 +80,7 @@ const Impressum = () => {
               <span className="font-medium">Amtsgericht Wuppertal</span><br /><br />
               Handelsregisternummer: <span className="font-medium">HRB 23380</span><br /><br />
               Vertretungsberechtigte Person:<br />
-              <span className="font-medium">Oliver Jacobi</span>, Geschäftsführer
+              <span className="font-medium">Oliver Jacobi</span>, Geschäftsführer & Gesellschafter
             </p>
           </section>
 
